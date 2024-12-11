c625f80db6535b7ac2c0760d01a8dcefbb22eadf
MohamadCS <mohamadessa443@gmail.com>
2024-12-12 00:34:12 +0200

    Documented the visitor namespace

--- include/prolog_antlr/Visitors.hpp
+++ include/prolog_antlr/Visitors.hpp
@@ -2,6 +2,7 @@
 
 #include "Parser.h"
 #include "ParserRuleContext.h"
+#include "RuleContext.h"
 #include "prologBaseVisitor.h"
 #include "prologParser.h"
 #include "tree/ParseTreeProperty.h"
@@ -14,12 +15,39 @@
 
 namespace Prolog::Visitors {
 
+/**
+ * @class FunctionSemanticsVisitor
+ * @brief Checks if the semantics of the functions are correct
+ *
+ */
 struct FunctionSemanticsVisitor : public prologBaseVisitor {
+
+    /**
+     * @brief A vector that stores the initialized variabls.
+     * An initialized variable, is an argument of the function, or a binded variable.
+     */
     std::vector<std::set<std::string>> initializedVars;
+
+    /**
+     * @brief Each entry in the vector is a for a different function, containing a map 
+     * that maps a variable to the number of bindings it has.
+     */
     std::vector<std::map<std::string, std::size_t>> bindedVars; // Func arg or binded.
 
+    /**
+     * @brief A set of the function names.
+     */
     std::set<std::string> functionNames;
+
+    /**
+     * @brief A set of invoked function, even if the function is not defined.
+     */
     std::set<std::string> functionInvoc;
+
+    /**
+     * @brief A list of expr's within a vanishing entry context.
+     */
+    std::list<antlr4::ParserRuleContext*> vanishingNoBinding; 
 
     std::any visitFunc_def(prologParser::Func_defContext* ctx) override;
     std::any visitBinding(prologParser::BindingContext* ctx) override;
@@ -30,17 +58,31 @@ struct FunctionSemanticsVisitor : public prologBaseVisitor {
 // TODO: Delete this class. 
 struct VariableSemanticVisitor : public prologBaseVisitor {
     std::any visitVariable(prologParser::VariableContext* ctx) override;
+
     std::map<std::string, std::uint8_t> varTbl;
     static constexpr std::size_t VAR_COUNT = 2;
 };
 
 struct MarkEmptyTuplesVisitor : public prologBaseVisitor {
+    /**
+     * @brief A property that gives wither a tuple is empty or not. 
+     * An empty tuple is '()', or a tuple of empty tuples. 
+     */
     antlr4::tree::ParseTreeProperty<bool> emptyTuples;
+
     std::any visitTuple(prologParser::TupleContext* ctx) override;
 };
 
 struct ProgramRestoreVisitor : public prologBaseVisitor {
+    /**
+     * @brief Each element of the list represents a stmt
+     * A stmt is a clause | directive | func_def
+     */
     std::list<std::list<std::string>> programStmtList = {{}};
+    /**
+     * @brief This is an optional property, if it is not nullopt
+     * then the empty tuples will be reduced.
+     */
     std::optional<antlr4::tree::ParseTreeProperty<bool>> emptyTuples;
 
     // NOTE: We can use std::variant for a cleaner code for clause/directive
